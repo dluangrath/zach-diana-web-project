6061716c7b15ecb5617a00f9257419133e7b141b
dluangrath <dluangrath9@gmail.com>
2022-01-10 19:18:51 -0800

    Co-authored-by: Zach Richter  <zach850@users.noreply.github.com>

--- app.js
+++ app.js
@@ -28,5 +28,21 @@ const months = [
   const format = document.querySelectorAll('.deadline-format h4');
   
 
-  let date = new Date(2022, 0, 11, 21, 00, 0);
-  console.log(date);
+  let futureDate = new Date(2022, 0, 11, 21, 00, 0);
+
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+
+  let month = futureDate.getMonth(); 
+  month = months[month];
+
+  const date = futureDate.getDate();
+
+  let day = futureDate.getDay();
+  day = weekdays[weekday];
+
+
+  giveaway.textContent = `Deadline is on ${date} ${month} ${year} ${hours}:${minutes}pm`;
+
+  

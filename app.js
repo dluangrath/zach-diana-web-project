5ba83b4193dbfdd0f2e6564c80047077753a65b5
dluangrath <dluangrath9@gmail.com>
2022-01-11 20:00:24 -0800

    second commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const months = [
 
   const giveaway = document.querySelector('.giveaway');
   const deadline = document.querySelector('.deadline');
-  const format = document.querySelectorAll('.deadline-format h4');
+  const stuff = document.querySelectorAll('.deadline-format h4');
   
   let futureDate = new Date(2022, 0, 11, 21, 00, 0);
 
@@ -43,10 +43,43 @@ const months = [
 
   // future time in MS 
   const futureTime = futureDate.getTime();
+
   function getRemainingTime() {
     const today = new Date().getTime();
+    const t = futureTime - today;
+
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60m
+  // 1d = 24hr
+  
+  // values in miliseconds
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    // calculate all values
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+
+    // values array
+    const values = [days, hours, minutes, seconds];
+    
+    function format(item) {
+      if (item < 10) {
+        return item = `0${item}`
+      }
+      return item
+    }
+
+    stuff.forEach(function(item, index) {
+      item.innerHTML = format(values[index]);
+    })
   }
 
-
-
+  // countdown
+  let countdown = setInterval(getRemainingTime, 1000);
   getRemainingTime();
